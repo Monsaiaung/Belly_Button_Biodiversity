6cc5d579821d171c68ae3096f2809a6e982c11c8
Mon <monsaiaung.mon@gmail.com>
2022-06-01 03:25:38 -0400

    add

--- charts.js
+++ charts.js
@@ -99,7 +99,7 @@ function buildCharts(sample) {
     // 9. Create the layout for the bar chart. 
     var barLayout = {
      title: "Top 10 Bacteria Cultures Found",
-     paper_bgcolor: "orangered1"
+     paper_bgcolor: "orangered2"
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", [barData], barLayout);
